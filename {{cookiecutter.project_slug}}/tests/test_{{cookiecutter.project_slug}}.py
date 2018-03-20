c49d608e50a66ddae81e83ca332fa05016ec73cf
Youngsung Kim <grnydawn@gmail.com>
2018-03-19 19:29:32 -0600

    added hello test for non-cli app

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.project_slug}}.py
@@ -34,6 +34,8 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
+    output = {{ cookiecutter.project_slug }}.hello('Peter')
+    assert 'Hello Peter' in output
 {%- if cookiecutter.command_line_interface|lower == 'click' %}
 
 
